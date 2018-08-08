4d954864f4582b902eaa97e044004af9d87c47cc
Alexander Boland <alexanderboland@gmail.com>
2018-08-08 18:45:39 -0400

    changing knexfile params

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@ module.exports = {
   connection: {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    host: process.env.CLEARDB_DATABASE_URL
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME
   }
 }
