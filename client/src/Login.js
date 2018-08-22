554808c364f2063a2ce3996d3e430fd5578d21b1
Alexander Boland <alexanderboland@gmail.com>
2018-08-22 13:59:22 -0400

    fixed login form to be less glitchy, added toggled-menu for mobile devices and appropriate media query

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Redirect } from 'react-router-dom'
+import formStyles from './stylesheets/formStyle.css'
 
 class Login extends React.Component {
 
-  state = {error: false, success: false};
+  state = {error: false, success: false, username: '', password: ''};
 
   constructor(props){
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setInfo = this.setInfo.bind(this);
+  }
+
+  setInfo (event, input) {
+    this.setState({[input]: event.target.value})
   }
 
   handleSubmit (event) {
     let self = this;
     event.preventDefault();
-    fetch('/session/login', { method: 'post', headers: {"Content-Type": "application/json"}, credentials: 'same-origin', body: JSON.stringify({ username: event.currentTarget[0].value, password: event.currentTarget[1].value }) })
+    fetch('/session/login', { method: 'post', headers: {"Content-Type": "application/json"}, credentials: 'same-origin',
+      body: JSON.stringify({ username: this.state.username, password: this.state.password }) })
       .then(response => response.json())
       .then(function(response) {
         if (response.success) {
@@ -27,20 +34,17 @@ class Login extends React.Component {
 
   render() {
     return <div>
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Username
-          <input type="text" name="username" />
-        </label>
-        <br />
-        <label>
-          Password
-          <input type="password" name="password" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      { this.state.error && <span>Invalid username/password combination</span> }
-      { this.state.success && <Redirect to='/' /> }
+      <h1>Login</h1>
+      <div className={formStyles.loginForm}>
+        <ul>
+          <li>Username: <input type="text" onChange={(evt) => {this.setInfo(evt, 'username')} }/></li>
+          <li>Password: <input type="password" onChange={(evt) => {this.setInfo(evt, 'password')}} /></li>
+          <li><button onClick={this.handleSubmit}>Submit</button></li>
+        </ul>
+
+        { this.state.error && <span>Invalid username/password combination</span> }
+        { this.state.success && <Redirect to='/' /> }
+      </div>
     </div>
   }
 }
